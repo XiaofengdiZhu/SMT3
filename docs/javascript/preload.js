64fe33a59b38637a8378ef9ef6514c8b497d15a0
销锋镝铸 <523084467@qq.com>
2018-07-21 02:09:15 +0800

    navigatorVersion加build时间

--- docs/javascript/preload.js
+++ docs/javascript/preload.js
@@ -1,4 +1,4 @@
-const navigatorVersion = "3.0.0";
+const navigatorVersion = "3.0.0 build 2018/7/21 2:05";
 const isMobile = (/(windows phone|symbianos|android|mobile|playbook|ipad|iphone|ipod)/i).test(navigator.userAgent);
 const isAndroidBrowser = isMobile?((/android/i).test(navigator.userAgent)):false;
 const webUrl = window.location.href;
